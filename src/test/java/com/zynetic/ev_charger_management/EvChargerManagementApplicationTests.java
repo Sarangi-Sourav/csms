46fc26222dba9d80e480b5619086dffd4489c01e
sarangi-sourav <souravsarangi2000@gmail.com>
2025-03-28 17:30:30 +0530

    initial commit

---
+++ src/test/java/com/zynetic/ev_charger_management/EvChargerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zynetic.ev_charger_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvChargerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
